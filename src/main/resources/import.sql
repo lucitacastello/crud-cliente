c398323732c9e852605e4f00be7a838b9428b9cc
Cida Rosa <lucita.rosa@gmail.com>
2023-06-05 18:25:38 -0300

    Seed DB, Entity Client, config application

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Claire Beauchamp Randall-Fraser', '14521478947', 6854.85, TIMESTAMP WITH TIME ZONE '1923-12-26T10:30:00Z', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('James MacKenzie Fraser', '95845632101', 3654.00, TIMESTAMP WITH TIME ZONE '1925-01-26T00:00:00Z', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Brianna Randall', '63521478920', 5847.15, TIMESTAMP WITH TIME ZONE '1943-05-05T22:30:00Z', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Jon Snow', '12345678900', 1200, TIMESTAMP WITH TIME ZONE '1986-12-26T10:30:00Z', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Daenerys Targaryen', '78512365498', 5350, TIMESTAMP WITH TIME ZONE '1995-07-20T10:30:00Z', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Jaime Lannister', '36945871265', 20154, TIMESTAMP WITH TIME ZONE '1995-01-01T00:30:00Z', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Cersei Lannister', '87456321457', 8946.36, TIMESTAMP WITH TIME ZONE '1991-09-13T20:50:00Z', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Arya Stark', '28431547893', 4578.01, TIMESTAMP WITH TIME ZONE '1988-03-27T20:50:07Z', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Sansa Stark', '39468751236', 5478, TIMESTAMP WITH TIME ZONE '1986-04-25T19:50:07Z', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Theon Greyjoy', '47813647896', 156987.65, TIMESTAMP WITH TIME ZONE '1987-06-25T12:50:10Z', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Joffrey Baratheon', '13479314587', 6547.36, TIMESTAMP WITH TIME ZONE '1991-08-15T03:30:00Z', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Tyrion Lannister', '54697421365', 20000, TIMESTAMP WITH TIME ZONE '1979-05-08T00:00:50Z', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Brienne de Tarth', '69714532698', 150458, TIMESTAMP WITH TIME ZONE '1976-11-29T14:30:00Z', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Ramsay Bolton', '89631456987', 58642.00, TIMESTAMP WITH TIME ZONE '1984-01-01T22:14:50Z', 0);
+
+
